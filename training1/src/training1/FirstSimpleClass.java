d7a8b408e3578705b06c32ae0ae69a2ffe855b4e
Saloni Modi <112194469+saloni-modi@users.noreply.github.com>
2024-07-22 19:15:00 +0530

    Add files via upload

---
+++ training1/src/training1/FirstSimpleClass.java
@@ -0,0 +1,15 @@
+package training1;
+
+public class FirstSimpleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java Training");
+		int x=3;
+		int y= 5;
+		float ans= (float)x/y;
+		System.out.println(ans);
+		
+	}
+
+}
